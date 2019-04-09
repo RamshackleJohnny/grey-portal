1613f0b6a761eb15f7e95691cbe70073ab7494a9
Johnny M <43150021+RamshackleJohnny@users.noreply.github.com>
2019-04-09 15:19:17 -0400

    Revert "Log in"

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,5 +8,5 @@ def test_config():
 def test_index(client):
     response = client.get('/')
     assert b'<h1>TSCT Portal</h1>' in response.data
-    assert b'<form action="/login" method="post">' in response.data
+    assert b'<form>' in response.data
 
