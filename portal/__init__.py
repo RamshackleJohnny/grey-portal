9848fa995bc2cdf6908e73e23e665b7da00ebc5a
Jonathan Marciano <bobthemoon@gmail.com>
2019-04-10 15:19:20 -0400

    - Session Id for login complete

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 
 import psycopg2
+import psycopg2.extras
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -21,13 +22,19 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # @app.before_request
+    # def before_request():
+        # if (not 'logged_in' in session) and (current_url != '/login'):
+            # return redirect(url_for('login'))
+        # pass
+
     @app.route('/', methods=['GET', 'POST'])
     def index():
         return render_template('index.html')
 
     @app.route('/dashboard', methods=['GET', 'POST'])
     def dash():
-        # print(session['user_id'])
+        print(session['user_id'])
         return render_template('dash.html')
 
     @app.route('/login', methods=['GET', 'POST'])
@@ -37,8 +44,6 @@ def create_app(test_config=None):
             print('Lets get it started!')
             email = request.form['email']
             password = request.form['password']
-            print(email)
-            print(password)
             connection = psycopg2.connect(database = "portal")
             cursor = connection.cursor()
             cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email,password,))
@@ -47,12 +52,12 @@ def create_app(test_config=None):
             if userdata == None:
                 return render_template('index.html', wrong=wrong)
             else:
-                cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email,password,))
-                # sesh = cursor.fetchone()
-                # print(sesh['id'])
-                # session.clear()
-                # session['user_id'] = users['id']
-
+                dict_cur = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                dict_cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email,password,))
+                sesh = dict_cur.fetchone()
+                session.clear()
+                session['user_id'] = sesh['id']
+                print(session['user_id'])
                 return redirect(url_for('dash'))
             print(userdata)
         return render_template('index.html')
