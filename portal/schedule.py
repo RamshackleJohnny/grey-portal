a3d1e3555de540f8cd2c7327e3ad83745da05dfa
Brian Gomez <briangx92@gmail.com>
2019-04-30 20:08:53 -0400

    It shows each student's individual session

--- portal/schedule.py
+++ portal/schedule.py
@@ -15,7 +15,7 @@ def schedule():
     # List of schedule for student
     with get_db() as con:
         with con.cursor() as cur:
-            cur.execute("SELECT session_id FROM user_sessions WHERE student_id = %s ORDER BY session_id ASC;", (current_student,))
+            cur.execute("SELECT course_name, time, description, session_id FROM user_sessions u_s JOIN course_sessions c_s ON c_s.id = u_s.session_id JOIN courses courses ON courses.course_id = c_s.course_id WHERE student_id = %s;", (current_student,))
             schedule_list = cur.fetchall()
 
     
